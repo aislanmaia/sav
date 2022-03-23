c0482893b15c14f4331f194e0b97be1e8928ce94
Aislan Maia <aislan.sousamaia@gmail.com>
2022-03-23 14:16:17 -0300

    Initialize hookstate and user store

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import '@hookstate/devtools'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App'
+import App from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
